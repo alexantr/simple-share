e244f1f933cb35572fcba0c5521760c353fe4172
Alex Yashkin <alex.yashkin@gmail.com>
2019-04-25 18:45:28 +0300

    Added new providers

--- simple-share.js
+++ simple-share.js
@@ -1,48 +1,81 @@
 ;(function ($) {
-    'use strict';
+    "use strict";
 
     var providers = {
-        'facebook': {
-            name: 'facebook',
-            link: 'https://www.facebook.com/sharer/sharer.php?u={url}',
-            width: 600,
-            height: 360,
-            title: 'Facebook'
+        // todo: add icons
+        "facebook": {
+            name: "facebook",
+            link: "https://www.facebook.com/sharer/sharer.php?u={url}",
+            title: "Facebook",
+            color: "#3b5998",
+            width: 800,
+            height: 520
         },
-        'twitter': {
-            name: 'twitter',
-            link: 'https://twitter.com/intent/tweet?url={url}&text={title}',
-            width: 600,
-            height: 255,
-            title: 'Twitter'
+        "vk": {
+            name: "vk",
+            link: "https://vk.com/share.php?url={url}&title={title}",
+            title: "VKontakte",
+            color: "#48729e",
+            width: 550,
+            height: 420
         },
-        'vk': {
-            name: 'vk',
-            link: 'https://vk.com/share.php?url={url}&title={title}',
-            width: 650,
-            height: 450,
-            title: 'VK'
+        "twitter": {
+            name: "twitter",
+            link: "https://twitter.com/intent/tweet?url={url}&text={title}",
+            title: "Twitter",
+            color: "#00aced",
+            width: 550,
+            height: 420
         },
-        'google-plus': {
-            name: 'google-plus',
-            link: 'https://plus.google.com/share?url={url}',
-            width: 500,
-            height: 550,
-            title: 'Google+'
+        "linkedin": {
+            name: "linkedin",
+            link: "https://www.linkedin.com/shareArticle?mini=true&url={url}&title={title}",
+            title: "LinkedIn",
+            color: "#0083be",
+            width: 800,
+            height: 520
         },
-        'pinterest': {
-            name: 'pinterest',
-            link: 'https://pinterest.com/pin/create/button/?url={url}&description={title}&media={image}',
-            width: 740,
-            height: 550,
-            title: 'Pinterest'
+        "odnoklassniki": {
+            name: "odnoklassniki",
+            link: "//ok.ru/dk?st.cmd=addShare&st._surl={url}&title={title}",
+            title: "Odnoklassniki",
+            color: "#eb722e",
+            width: 800,
+            height: 520
         },
-        'linkedin': {
-            name: 'linkedin',
-            link: 'https://www.linkedin.com/shareArticle?mini=true&url={url}&title={title}',
-            width: 600,
-            height: 400,
-            title: 'LinkedIn'
+        "pinterest": {
+            name: "pinterest",
+            link: "https://pinterest.com/pin/create/button/?url={url}&description={title}&media={image}",
+            title: "Pinterest",
+            color: "#c20724",
+            width: 800,
+            height: 520
+        },
+        "viber": {
+            name: "viber",
+            link: "viber://forward?text={title}%20{url}",
+            title: "Viber",
+            color: "#7b519d"
+        },
+        "whatsapp": {
+            name: "whatsapp",
+            link: "whatsapp://send?text={title}%20{url}",
+            title: "WhatsApp",
+            color: "#65bc54"
+        },
+        "telegram": {
+            name: "telegram",
+            link: "https://telegram.me/share/url?url={url}&text={title}",
+            title: "Telegram",
+            color: "#64a9dc"
+        },
+        "skype": {
+            name: "skype",
+            link: "https://web.skype.com/share?url={url}",
+            title: "Skype",
+            color: "#00aff0",
+            width: 800,
+            height: 520
         }
     };
 
@@ -50,29 +83,24 @@
 
         opt = $.extend(true, {
             providers: providers,
-            url: window.location.href.replace(window.location.hash, ''),
-            title: (document.title || ''),
-            image: '',
+            url: window.location.href.replace(window.location.hash, ""),
+            title: (document.title || ""),
+            image: "",
             enabledProviders: [],
-            wrapperTemplate: '{buttons}',
-            buttonTemplate: '<span title="{title}" class="icon-{name}" onclick="{onclick}"></span>',
-            onclickTemplate: "window.open('{link}','_blank','scrollbars=0,resizable=1,menubar=0,left=100,top=100,width={width},height={height},toolbar=0,status=0');return false;",
-            removeContainer: false,
-            popupWidth: 550,
-            popupHeight: 440
+            classPrefix: "simple-share-"
         }, opt || {});
 
         return this.each(function () {
 
             var $el = $(this), options = $.extend(true, opt, dataToOptions($el));
-            var j, link, onclick, wWidth, wHeight, pTitle, providers = [], html = '';
+            var j, providers = [];
 
             // enable default providers
             if (!options.enabledProviders.length) {
-                options.enabledProviders = ['facebook', 'twitter', 'vk'];
+                options.enabledProviders = ["facebook", "twitter", "vk"];
             }
 
-            if (typeof options.enabledProviders === 'string') {
+            if (typeof options.enabledProviders === "string") {
                 options.enabledProviders = options.enabledProviders.split(/\s*,\s*/);
             }
 
@@ -85,23 +113,33 @@
 
             // add buttons
             for (j = 0; j < providers.length; j++) {
-                if (!('link' in providers[j]) || !('name' in providers[j]))
+                if (!("link" in providers[j]) || !("name" in providers[j]))
                     continue;
                 // pinterest must have not-empty media param
-                if (providers[j].name === 'pinterest' && !options.image)
+                if (providers[j].name === "pinterest" && !options.image)
                     continue;
-                link = makeUrl(providers[j].link, {'url': options.url, 'title': options.title, 'image': options.image});
-                wWidth = ('width' in providers[j]) ? providers[j].width : options.popupWidth;
-                wHeight = ('height' in providers[j]) ? providers[j].height : options.popupHeight;
-                pTitle = ('title' in providers[j]) ? providers[j].title : '';
-                onclick = template(options.onclickTemplate, {'link': link, 'width': wWidth, 'height': wHeight});
-                html += template(options.buttonTemplate, {'title': pTitle, 'name': providers[j].name, 'onclick': onclick});
-            }
-
-            if (options.removeContainer) {
-                $el.replaceWith(template(options.wrapperTemplate, {'buttons': html}));
-            } else {
-                $el.html(template(options.wrapperTemplate, {'buttons': html}));
+                var link = makeUrl(providers[j].link, {"url": options.url, "title": options.title, "image": options.image});
+                var pTitle = ("title" in providers[j]) ? providers[j].title : "";
+                var $a = $('<a/>');
+                $a.attr('href', link);
+                $a.attr('title', pTitle);
+                $a.attr('rel', 'noopener');
+                $a.addClass(options.classPrefix + providers[j].name);
+                if ("width" in providers[j] && "height" in providers[j]) {
+                    $a.attr('data-width', providers[j].width);
+                    $a.attr('data-height', providers[j].height);
+                    $a.on('click', function (e) {
+                        e.preventDefault();
+                        window.open(
+                            $(this).attr('href'),
+                            '_blank',
+                            'scrollbars=0,resizable=1,menubar=0,left=100,top=100,width=' + $(this).data('width') + ',height=' + $(this).data('height') + ',toolbar=0,status=0'
+                        );
+                    });
+                } else {
+                    $a.attr('target', '_blank');
+                }
+                $a.appendTo($el);
             }
         });
     };
@@ -118,10 +156,10 @@
             if (!data.hasOwnProperty(key))
                 continue;
             var value = data[key];
-            if (value === 'yes' || value === 'true') {
+            if (value === "yes" || value === "true") {
                 value = true;
             }
-            else if (value === 'no' || value === 'false') {
+            else if (value === "no" || value === "false") {
                 value = false;
             }
             options[key.replace(/-(\w)/g, upper)] = value;
